7fa83350c709697dbb7fc1b8b4aa2f30efedbf45
OMatv <149325870+OMatv@users.noreply.github.com>
2024-09-08 20:59:50 +0300

    fixed3

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,8 +1,6 @@
-const errorHandler = (error, req, res, next) => {
+export default function errorHandler(error, req, res, next) {
   const { status = 500, message } = error;
   res.status(status).json({
     message,
   });
-};
-
-export default errorHandler;
+}
--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,7 +1,5 @@
-const notFoundHandler = (req, res) => {
+export default function notFoundHandler(req, res) {
   res.status(404).json({
     message: `${req.url} not found`,
   });
-};
-
-export default notFoundHandler;
+}
