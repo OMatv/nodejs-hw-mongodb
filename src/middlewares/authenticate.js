7dacb18d9634ad716c340cdb208e15e4294203fd
Olha Matviienko <olmatv09@gmail.com>
2024-10-28 15:11:24 +0100

    6

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,7 +1,7 @@
 import createHttpError from 'http-errors';
 
 import  UsersCollection from  "../db/models/user.js";
-import  SessionsCollection  from '../db/models/session.js';
+import  SessionsCollection  from "../db/models/session.js";
 
 export const authenticate = async (req, res, next) => {
   const authHeader = req.get('Authorization');
